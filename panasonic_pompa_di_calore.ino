1bf16deaed0635deb1fce577dd848b1885b64ee0
Domenico Ceccarelli <37450907+riddik14@users.noreply.github.com>
2019-06-29 12:32:11 +0200

    Update panasonic_pompa_di_calore.ino

--- panasonic_pompa_di_calore.ino
+++ panasonic_pompa_di_calore.ino
@@ -1,4 +1,4 @@
-/*progetto creato da Domenico Ceccarelli - Italia
+/*progetto creato das Domenico Ceccarelli - Italia
 offritemi un caffe
 
 progetto testato su un condizionatore PANASONIC
@@ -13,13 +13,17 @@ https://www.paypal.me/DomenicoCeccarelli
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 
-#define ssid ""
-#define password ""
-#define mqtt_server ""
-#define mqtt_port 1883
-#define mqtt_user ""
-#define mqtt_pass ""
-#define mqtt_client ""
+
+//wlan setup DHCP ON
+#define ssid "wlan2"
+#define password "mimmoelisabettalucia"
+
+//impostazioni mqtt
+#define mqtt_server "192.168.1.247" //server
+#define mqtt_port 1883              //porta
+#define mqtt_user "admin"           //username
+#define mqtt_pass "mimmo14"         //password
+#define mqtt_client "IRremote"      //nome client esp8266
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -31,35 +35,24 @@ String strPayload;
 #ifndef UNIT_TEST
 #include <Arduino.h>
 #endif
+
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <ir_Panasonic.h>
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin  4 (D2) ir TX.
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin  4 (D2) modulo ir TX.
 IRPanasonicAc ac(kIrLed);
 
-void printState() {
-  // Display the settings.
-  Serial.println("Panasonic A/C remote è:");
-  Serial.printf("  %s\n", ac.toString().c_str());
-  // Display the encoded IR sequence.
-  unsigned char* ir_code = ac.getRaw();
-  Serial.print("Codice IR inviato: 0x");
-  for (uint8_t i = 0; i < kPanasonicAcStateLength; i++)
-    Serial.printf("%02X", ir_code[i]);
-  Serial.println();
-}
+
 
 void setup() {
   Serial.begin(115200);
   delay(200);
-  ac.begin();
-
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  Serial.println("imposto di default il condizionatore");
-  printState();
-
+  Serial.println("invio il primo comando di spegnimento del condizionatore");
+  printState_init();  
+  ac.begin();
 }
 
 void loop() {
@@ -68,4 +61,3 @@ void loop() {
   }
   client.loop();
 }
-
