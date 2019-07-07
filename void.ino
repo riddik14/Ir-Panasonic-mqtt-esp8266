8b6dd9a273a6b3ca370fa7bc95c4d6e2407bca90
Domenico Ceccarelli <37450907+riddik14@users.noreply.github.com>
2019-07-07 19:02:19 +0200

    Update void.ino

--- void.ino
+++ void.ino
@@ -41,57 +41,6 @@ void printState_init() {
     Serial.printf("%02X", ir_code[i]);
   Serial.println();
 }
-void callback(char* topic, byte* payload, unsigned int length) {
-  payload[length] = '\0';
-  strTopic = String((char*)topic);
-  if (strTopic == mqtt_topic)
-  {
-    ac_panasonic = String((char*)payload);
-
-
-    if (ac_panasonic == "ONcool")
-    {
-      Serial.println("ON_cool");
-      ac_on_cold ();
-    }
-    if (ac_panasonic == "OFFcool")
-    {
-      Serial.println("OFF_cool");
-      ac_off_cold ();
-    }
-    if (ac_panasonic == "ONhot")
-    {
-      Serial.println("ON_hot");
-      ac_on_hot ();
-    }
-    if (ac_panasonic == "OFFhot")
-    {
-      Serial.println("OFF_hot");
-      ac_off_hot ();
-    }
-    if (ac_panasonic == "ONdry")
-    {
-      Serial.println("ON_dry");
-      ac_on_dry ();
-    }
-    if (ac_panasonic == "OFFdry")
-    {
-      Serial.println("OFF_dry");
-      ac_off_dry ();
-    }
-    if (ac_panasonic == "OFFauto")
-    {
-      Serial.println("OFF_auto");
-      ac_off_auto ();
-    }
-    if (ac_panasonic == "ONauto")
-    {
-      Serial.println("ON_auto");
-      ac_on_auto ();
-    }
-  }
-}
-
 
 void reconnect() {
   while (!client.connected()) {
