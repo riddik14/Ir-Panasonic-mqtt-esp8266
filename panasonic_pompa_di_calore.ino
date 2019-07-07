ba78629e5739dda736524fbe95b76068f591d956
Domenico Ceccarelli <37450907+riddik14@users.noreply.github.com>
2019-07-07 18:56:36 +0200

    new version

--- panasonic_pompa_di_calore.ino
+++ panasonic_pompa_di_calore.ino
@@ -5,6 +5,12 @@ progetto testato su un condizionatore PANASONIC
 malwar@hotmail.it
 https://www.paypal.me/DomenicoCeccarelli
 
+
+default topic
+mqtt topic temperatura" ha/ac_panasonic/temperature"
+mqtt topic humidity "ha/ac_panasonic/humidity"
+mqtt topic command  "ha/ac_panasonic"     playload "on_auto_Mode_temp_auto_auto"          ******esempio "on_auto_Cool_16_auto_auto acceso-modo-auto-16gradi-autoswingh-autoswingv"
+
 */
 
 #include <ESP8266WiFi.h>
@@ -13,16 +19,15 @@ https://www.paypal.me/DomenicoCeccarelli
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 
-
 //wlan setup DHCP ON
-#define ssid "wlan2"
-#define password "mimmoelisabettalucia"
+#define ssid ""
+#define password ""
 
 //impostazioni mqtt
-#define mqtt_server "192.168.1.247" //server
+#define mqtt_server "" //server
 #define mqtt_port 1883              //porta
-#define mqtt_user "admin"           //username
-#define mqtt_pass "mimmo14"         //password
+#define mqtt_user ""           //username
+#define mqtt_pass ""         //password
 #define mqtt_client "IRremote"      //nome client esp8266
 #define mqtt_topic "ha/ac_panasonic" //inserisci il topic
 
@@ -41,13 +46,25 @@ String strPayload;
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <ir_Panasonic.h>
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin  4 (D2) modulo ir TX.
+const uint16_t kIrLed = 4;     // ESP8266 GPIO pin  4 (D2) modulo ir TX.
 IRPanasonicAc ac(kIrLed);
 
+#include "DHT_U.h"
+#define DHTPIN 2              // DHT Pin (D4)
+//#define DHTTYPE DHT11       // DHT 11 
+#define DHTTYPE DHT22         // DHT 22  (AM2302)
+#define temperature_topic "ha/ac_panasonic/temperature"  //Topic temperature
+#define humidity_topic "ha/ac_panasonic/humidity"        //Topic humidity
+DHT dht(DHTPIN, DHTTYPE);    
+char message_buff[100];
 
+long lastMsg = 0;   
+long lastRecu = 0;
+bool debug = false;  
 
 void setup() {
   Serial.begin(115200);
+  
   delay(200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -58,6 +75,8 @@ void setup() {
   //Serial.println("Invio il primo comando di spegnimento del condizionatore");
   //printState_init();  
   ac.begin();
+  dht.begin();
+  
 }
 
 void loop() {
@@ -65,5 +84,33 @@ void loop() {
     reconnect();
   }
   client.loop();
-}
+  long now = millis();
+  if (now - lastMsg > 100 * 60) { //tempo invio temperatura
+    lastMsg = now;
+    // Read humidity
+    float h = dht.readHumidity();
+    // Read temperature in Celcius
+    float t = dht.readTemperature();
 
+    // Oh, nothing to send
+    if ( isnan(t) || isnan(h)) {
+      Serial.println("attenzione sensore non connesso");
+      return;
+    }
+  
+    if ( debug ) {
+      Serial.print("Temp : ");
+      Serial.print(t);
+      Serial.print(" | Hum : ");
+      Serial.println(h);
+    }  
+    client.publish(temperature_topic, String(t).c_str(), true);   // Publish temperature on temperature_topic
+    client.publish(humidity_topic, String(h).c_str(), true);      // and humidity
+    Serial.print("Temperatura : ");   Serial.print(t); Serial.println(" °C");
+    Serial.print(" | Umidita : ");   Serial.print(h); Serial.println(" %");
+  }
+  if (now - lastRecu > 100 ) {
+    lastRecu = now;
+    client.subscribe("ha/ac_panasonic");
+  }
+}
