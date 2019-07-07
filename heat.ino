5084141084739b8009c3f13b7825b75ff586765e
Domenico Ceccarelli <37450907+riddik14@users.noreply.github.com>
2019-07-07 19:01:05 +0200

    Create heat.ino

---
+++ heat.ino
@@ -0,0 +1,255 @@
+void on_auto_Heat_16_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(16);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 16...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_17_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(17);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 17...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_18_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(18);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 18...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_19_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(19);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 19...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_20_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(20);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 20...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_21_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(21);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 21...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_22_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(22);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 22...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_23_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(16);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 23...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_24_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(24);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 24...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_25_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(25);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 25...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_26_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(26);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 26...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_27_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(27);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 27...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_28_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(28);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 28...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_29_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(16);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 29...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
+void on_auto_Heat_30_auto_auto () {
+  {
+    ac.setModel(kPanasonicRkr);
+    ac.on();
+    ac.setFan(kPanasonicAcFanAuto);
+    ac.setMode(kPanasonicAcHeat);
+    ac.setTemp(30);
+    ac.setSwingVertical(kPanasonicAcSwingVAuto);
+    ac.setSwingHorizontal(kPanasonicAcSwingHAuto);
+  }
+#if SEND_PANASONIC_AC
+  Serial.println(" temp 30...power-on... mode caldo... ventola AUTO... swing verticale AUTO... swing orizzontale AUTO...");
+  ac.send();
+#endif
+  printState();
+  Serial.println("invio effettuato");
+}
